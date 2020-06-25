9cfde24183dfa7894c44476af69105f24ade0eff
SAlvesJr <jr-alves@outlook.com>
2020-06-25 16:15:53 -0300

    documentacao swagger

--- src/main/java/com/SAlvesJr/webService/config/SecurityConfig.java
+++ src/main/java/com/SAlvesJr/webService/config/SecurityConfig.java
@@ -31,13 +31,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JWTUtil jwtUtil;
 
-	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/swagger-ui.html" };
 
-	private static final String[] PUBLIC_MATCHERS_GET = { "/characters/**", "/comics/**"};
+	private static final String[] PUBLIC_SWAGGER = { "/v2/api-docs/**", "/swagger.json", "/swagger-ui.html",
+			"/swagger-resources/**", "/webjars/**", "/csrf" };
+
+	private static final String[] PUBLIC_MATCHERS_GET = { "/characters/**", "/comics/**" };
 
 	private static final String[] PUBLIC_MATCHERS_POST = { "/clientes", "/characters/**" };
 
@@ -49,12 +52,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
-				.anyRequest().authenticated();
+				.antMatchers(PUBLIC_SWAGGER).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
